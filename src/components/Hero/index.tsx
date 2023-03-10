21a8d350dd76dec4870772b11367f13c86b075b4
hali241997 <mhali241997@gmail.com>
2023-03-10 15:56:10 +0500

    Update hero image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -8,7 +8,7 @@ const Hero: FC = () => {
         <img
           src="/images/hero.png"
           alt="hero"
-          className="object-cover h-[600px]"
+          className="object-cover h-[600px] border-bluePrimary border-[2px] rounded"
         />
       </div>
 
