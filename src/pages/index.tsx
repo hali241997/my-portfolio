7209317fb5db48326532175cb21716a10da0eb84
hali241997 <mhali241997@gmail.com>
2023-03-24 16:15:09 +0500

    Add favicon and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "components/Header";
 import Hero from "components/Hero";
 import Portfolio from "components/Portfolio";
 import TechStack from "components/TechStack";
+import Head from "next/head";
 import { FC, useEffect, useRef } from "react";
 
 const HomePage: FC = () => {
@@ -36,6 +37,36 @@ const HomePage: FC = () => {
 
   return (
     <>
+      <Head>
+        <title>Hasnain Ali - Providing Full-Stack Development Services</title>
+        <meta
+          name="description"
+          content="Providing Full-Stack Development Services"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Header
         aboutMeRef={aboutMeRef}
         portfolioRef={portfolioRef}
