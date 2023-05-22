da08c159f433ccec8b54ada748a976f2109a5647
hali241997 <mhali241997@gmail.com>
2023-05-22 16:44:17 +0500

    Add br

--- src/pages/__admin.tsx
+++ src/pages/__admin.tsx
@@ -3,7 +3,19 @@ import { FC } from "react";
 const Admin: FC = () => {
   return (
     <div>
-      <h1>Please contact me here for a discounted price: +923217738854</h1>
+      <h1>
+        Please contact me on whatsapp for a discounted price: +923217738854
+      </h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
