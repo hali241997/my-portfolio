53e03bc8dbac45ce5fbd57547afa4098a94d3ea4
hali241997 <mhali241997@gmail.com>
2023-05-22 16:46:01 +0500

    fix

--- src/pages/__admin.tsx
+++ src/pages/__admin.tsx
@@ -3,7 +3,12 @@ import { FC } from "react";
 const Admin: FC = () => {
   return (
     <div>
-      <h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1 className="text-[50px]">
         Please contact me on whatsapp for a discounted price: +923217738854
       </h1>
       <br />
