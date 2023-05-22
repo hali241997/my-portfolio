0da2f008985991c3655114a66f00611f404eb4f3
hali241997 <mhali241997@gmail.com>
2023-05-22 16:42:05 +0500

    dummy page

---
+++ src/pages/__admin.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Admin: FC = () => {
+  return (
+    <div>
+      <h1>Please contact me here for a discounted price: +923217738854</h1>
+    </div>
+  );
+};
+
+export default Admin;
