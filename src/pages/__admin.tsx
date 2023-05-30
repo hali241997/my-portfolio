9948b5fcb577625f38d620b37742a5046967b839
hali241997 <mhali241997@gmail.com>
2023-05-31 00:41:04 +0500

    test

---
+++ src/pages/__admin.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Admin: FC = () => {
+  return (
+    <div className="h-[500px] flex flex-1 items-center justify-center">
+      <div className="text-[48px]">
+        Pleae contact me on Whatsapp for a discounted price: +923217738854
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
