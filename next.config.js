df1b8b71808221654bd36cda190b73e4d2b49c23
hali241997 <mhali241997@gmail.com>
2023-06-04 15:15:00 +0500

    Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new (require("webpack").IgnorePlugin)(/__admin\.tsx$/)
+      );
+    }
+  },
 };
 
 module.exports = nextConfig;
