ce2423f0f90898859bc1b031351840b81b40f706
hali241997 <mhali241997@gmail.com>
2023-06-05 02:53:34 +0500

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["_app.tsx", "_document.tsx", "index.tsx"],
 };
 
 module.exports = nextConfig;
--- src/components/__admin.tsx
+++ src/components/__admin.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Admin: FC = () => {
   return (
-    <div className="h-[500px] flex flex-1 items-center justify-center">
+    <div className=" h-[500px] flex flex-1 items-center justify-center">
       <div className="text-[48px]">
         Pleae contact me on Whatsapp for a discounted price: +923217738854
       </div>
