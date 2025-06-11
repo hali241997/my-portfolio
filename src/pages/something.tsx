7c7c4849c7631e45cdc2a2991873e675b486986f
Hasnain Ali <h.ali@automotive-ai.com>
2025-06-12 02:05:44 +0500

    add

---
+++ src/pages/something.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Something: FC = () => {
+  return (
+    <div>
+      <h1>You can contact me here:</h1>
+      <p>Whatsapp: +923217738854</p>
+    </div>
+  );
+};
+
+export default Something;
